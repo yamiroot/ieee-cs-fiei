a0fd26907b7559aeaf80d44d92ff2a566647b843
yamiroot <yamipqm@gmail.com>
2020-10-04 12:17:38 -0500

    modales founders

--- unfold-free-lite/assets/js/modal.js
+++ unfold-free-lite/assets/js/modal.js
@@ -1,75 +1,59 @@
-const $$ = {
-    buttonCloseModal: () => {
-        const buttonsClose = doc.querySelectorAll('.founders');
 
-        buttonsClose.forEach((buttonClose) => {
-            buttonClose.addEventListener('click', () => {
-                deleteNode(document.querySelector('.modal'));
+const dataFounders = {
+    arturoBadillo: {
+        name: 'Arturo Badillo',
+        img: './assets/images/history_cs/arturo-badillo.jpeg',
+        alt: 'Arturo Badillo',
+        position: '1er presidente',
+        ocupation: '',
+        commentary: 'GGGGGGGGGGGG'
+    },
+    victorMiranda: {
+        name: 'Victor Miranda',
+        img: './assets/images/history_cs/victor-miranda.png',
+        alt: 'Victor Miranda',
+        position: '1er secretario',
+        ocupation: '',
+        commentary: ''
+    }
+}
+
+const $$ = {
+    buttonModalFounder: () => {
+        const buttonsFounder = document.querySelectorAll('.button-founder');
+
+        buttonsFounder.forEach((buttonFounder) => {
+            buttonFounder.addEventListener('click', (event) => {
+                
+                const modalTitle = document.getElementById('title-founder-modal');
+                const modalImgFounder = document.getElementById('div-img-modal').children[0];
+                const modalPositionFounder = document.getElementById('div-position-founder-modal');
+                const modalOcupationFounder = document.querySelector('#div-ocupation-founder-modal');
+                const modalCommentaryFounder = document.querySelector('#div-commentary-founder-modal');
+                console.log(event.target.dataset.val)
+
+                switch (event.target.dataset.val) {
+                    case 'arturo-badillo':
+                        modalTitle.textContent = dataFounders.arturoBadillo.name;
+                        modalImgFounder.src = dataFounders.arturoBadillo.img;
+                        modalPositionFounder.textContent = dataFounders.arturoBadillo.position;
+                        modalOcupationFounder.textContent = dataFounders.arturoBadillo.ocupation;
+                        modalCommentaryFounder.innerHTML = dataFounders.arturoBadillo.commentary;
+
+                        break;
+                    case 'victor-miranda':
+                        modalTitle.textContent = dataFounders.victorMiranda.name;
+                        modalImgFounder.src = dataFounders.victorMiranda.img;
+                        modalPositionFounder.textContent = dataFounders.victorMiranda.position;
+                        modalOcupationFounder.textContent = dataFounders.victorMiranda.ocupation;
+                        modalCommentaryFounder.textContent = dataFounders.victorMiranda.commentary;
+                        break;
+                    case '':
+                        break;
+                }
             });
         });
     },
 };
 
-fnGeneratorContentModal = (pClassBody, pExistsHeader, pTemplates, pIsCamera) => {
-    const divContent = document.createElement('div');
-    divContent.className = 'modal-content';
-
-    if (pExistsHeader) {
-        const divHeader = document.createElement('div');
-        divHeader.className = 'modal-header';
-
-        if (!pIsCamera) {
-            divHeader.innerHTML = `
-                <h6 class="modal-title">${pTemplates.header}</h6>
-                <button type="button" class="close button-close" aria-label="Close">
-                    <span>&times;</span>
-                </button>`;
-
-            divContent.appendChild(divHeader);
-        } else {
-            divHeader.innerHTML = `
-                <select name="device" id="devicesList" class="form-control anchoInput fontSize">Dispositivo</select>
-                <button type="button" class="close button-close" aria-label="Close">
-                    <span>&times;</span>
-                </button>`;
-
-            divContent.appendChild(divHeader);
-        }
-    }
-
-    const divBody = document.createElement('div');
-    divBody.className = `modal-body ${pClassBody}`;
-    divBody.innerHTML = pTemplates.body;
-
-    divContent.appendChild(divBody);
-
-    const divFooter = document.createElement('div');
-    divFooter.className = 'modal-footer';
-    divFooter.innerHTML = pTemplates.footer;
-
-    divContent.appendChild(divFooter);
-
-    return divContent;
-}
-
-fnGeneratorModal = (pClassModal, pClassBody, pExistsHeader, pTemplates, pIsCamera) => {
-    const divContainer = document.createElement('div');
-    divContainer.className = 'modal d-flex justify-content-center align-items-center show';
-    divContainer.tabIndex = '-1';
-    divContainer.setAttribute('aria-hidden', 'true');
-    divContainer.setAttribute('role', 'dialog');
-
-    const divModal = divContainer.cloneNode('div');
-    divModal.className = pClassModal;
-    divModal.role = 'document';
-
-    divModal.appendChild(fnGeneratorContentModal(pClassBody, pExistsHeader, pTemplates, pIsCamera));
-    divContainer.appendChild(divModal);
-    document.body.appendChild(divContainer);
-
-    window.addEventListener('click', (event) => {
-        const containerModal = document.querySelector('.modal');
-
-        if (event.target == containerModal) containerModal.remove();
-    });
-}
+$$.buttonModalFounder();
